0e2f57317e34a860bbf50c39e9f376f58a7966f2
Rango Yuan <Rango_Yuan@epam.com>
2019-10-31 22:05:10 +0800

    update file name

---
+++ src/Ganic.js
@@ -0,0 +1,18 @@
+const SINGLETON = require('./etc/singleton')
+const orphanage = require('./etc/orphanage')
+
+exports.take = function(deps) {
+  if (!SINGLETON.operatingOrgan) throw "Don't use TAKE outside of organism!"
+  return SINGLETON.operatingOrgan.take(deps)
+}
+
+const orphanLive = (organism, props) => {
+  const organ = orphanage.get(organism)
+  organ.receiveProps(props)
+  return organ
+}
+
+exports.live = function(organism, props, parent, key) {
+  if (!parent) return orphanLive(organism, props)
+  // todo, add parent and key logic
+}

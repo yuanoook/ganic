f921c4b2dc2f5d600f0ac1db3548ac516d657eea
Rango Yuan <yuanoook@foxmail.com>
2019-11-02 12:58:48 +0800

    inprove debounce-throttle test

--- src/use/__test__/debounce-throttle.test.js
+++ src/use/__test__/debounce-throttle.test.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const {create} = require('../../Ganic');
-
 const {
   useState,
   useInterval,
@@ -10,8 +9,8 @@ const {
   useDebounce,
   useThrottle,
 } = require('..');
-
 const {checkAsyncExpectation} = require('./utils');
+const Lakhesis = require('../../moirai/Lakhesis');
 
 /**
  * WARNING:
@@ -21,7 +20,7 @@ const {checkAsyncExpectation} = require('./utils');
  *      - https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/setTimeout#Reasons_for_delays_longer_than_specified
  *
  * TODO:
- *    Figure out a way to inspect which parasite made which update
+ *    Figure out a better way to test this
  */
 
 describe('useDebounce & useThrottle', () => {
@@ -30,8 +29,12 @@ describe('useDebounce & useThrottle', () => {
 
   it('should debounce state updates', done => {
     const organism = () => {
-      const [state, setState] = useState(0);
-      const [interval, setInterval] = useState(50);
+      const [state, setState] = useState(0);        // parasite index 0
+      const [interval, setInterval] = useState(50); // parasite index 1
+
+      // bring debouncedState, 60ms after the last state update
+      // expect it will skip two updates, and bring out the final state
+      const debouncedState = useDebounce(state, 60);// parasite index 2
 
       // start updating the state every 50ms
       useInterval(() => setState(n => n + 1), interval);
@@ -40,29 +43,28 @@ describe('useDebounce & useThrottle', () => {
       // expect there'll be two updates only
       useTimeout(() => setInterval(null), 225);
 
-      // bring debouncedState, 60ms after the last state update
-      // expect it will skip two updates, and bring out the final state
-      const debouncedState = useDebounce(state, 60);
-      return {state, debouncedState, interval};
+      const givingParasiteIndex = Lakhesis.givingParasite && Lakhesis.givingParasite.index;
+
+      return {state, debouncedState, interval, givingParasiteIndex};
     };
 
     const expectation = [
       // 0ms - init
-      [{state: 0, debouncedState: 0, interval: 50}],
+      [{state: 0, debouncedState: 0, interval: 50, givingParasiteIndex: null}],
 
       // 50ms - skipped the 1st update from useInterval
-      [{state: 1, debouncedState: 0, interval: 50}],
+      [{state: 1, debouncedState: 0, interval: 50, givingParasiteIndex: 0}],
 
       // - skipped 3 more updates
-      [{state: 2, debouncedState: 0, interval: 50}],
-      [{state: 3, debouncedState: 0, interval: 50}],
-      [{state: 4, debouncedState: 0, interval: 50}],
+      [{state: 2, debouncedState: 0, interval: 50, givingParasiteIndex: 0}],
+      [{state: 3, debouncedState: 0, interval: 50, givingParasiteIndex: 0}],
+      [{state: 4, debouncedState: 0, interval: 50, givingParasiteIndex: 0}],
 
       // 225ms - ignored update from useTimeout, interval timer stoped here
-      [{state: 4, debouncedState: 0, interval: null}],
+      [{state: 4, debouncedState: 0, interval: null, givingParasiteIndex: 1}],
 
       // 260ms (4 * 50ms + 60ms) - bring out the final state
-      [{state: 4, debouncedState: 4, interval: null}],
+      [{state: 4, debouncedState: 4, interval: null, givingParasiteIndex: 2}],
     ];
 
     const organ = create({organism});
@@ -71,30 +73,34 @@ describe('useDebounce & useThrottle', () => {
 
   it('should throttle state updates', done => {
     const organism = () => {
-      const [state, setState] = useState(0);
+      const [state, setState] = useState(0);          // parasite index 0
+      const throttledState = useThrottle(state, 135); // parasite index 1
+
       useInterval(() => setState(n => n + 1), 50);
-      const throttledState = useThrottle(state, 135);
-      return {state, throttledState};
+
+      const givingParasiteIndex = Lakhesis.givingParasite && Lakhesis.givingParasite.index;
+
+      return {state, throttledState, givingParasiteIndex};
     };
 
     const expectation = [
       // 0ms - init
-      [{state: 0, throttledState: 0}],
+      [{state: 0, throttledState: 0, givingParasiteIndex: null}],
 
       // 50ms - 100ms - throttled 2 updates
-      [{state: 1, throttledState: 0}],
-      [{state: 2, throttledState: 0}],
+      [{state: 1, throttledState: 0, givingParasiteIndex: 0}],
+      [{state: 2, throttledState: 0, givingParasiteIndex: 0}],
 
       // 135ms - brought out the 2nd update
-      [{state: 2, throttledState: 2}],
+      [{state: 2, throttledState: 2, givingParasiteIndex: 1}],
 
       // 150ms - 250ms - throttled 3 more updates
-      [{state: 3, throttledState: 2}],
-      [{state: 4, throttledState: 2}],
-      [{state: 5, throttledState: 2}],
+      [{state: 3, throttledState: 2, givingParasiteIndex: 0}],
+      [{state: 4, throttledState: 2, givingParasiteIndex: 0}],
+      [{state: 5, throttledState: 2, givingParasiteIndex: 0}],
 
       // 270ms - brought out the 5th update
-      [{state: 5, throttledState: 5}],
+      [{state: 5, throttledState: 5, givingParasiteIndex: 1}],
     ];
 
     const organ = create({organism});

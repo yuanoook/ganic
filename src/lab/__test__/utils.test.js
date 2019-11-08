d86505258e8731c87d48eb15b5d890aca574905f
Rango Yuan <Rango_Yuan@epam.com>
2019-11-08 13:10:37 +0800

    update shallowEqual null === undefined

--- src/lab/__test__/utils.test.js
+++ src/lab/__test__/utils.test.js
@@ -1,8 +1,27 @@
 'use strict';
 
-const {flat} = require('../utils');
+const {
+  shallowEqual,
+  flat,
+} = require('../utils');
 
 describe('lab utils', () => {
+  it('shallowEqual', () => {
+    expect(shallowEqual({}, {})).toBe(true);
+    expect(shallowEqual(null, null)).toBe(true);
+    expect(shallowEqual(undefined, undefined)).toBe(true);
+    expect(shallowEqual(null, undefined)).toBe(true);
+    expect(shallowEqual([], [])).toBe(true);
+    expect(shallowEqual({a: 1}, {a: 1})).toBe(true);
+
+    const b = {};
+    expect(shallowEqual({a: b}, {a: b})).toBe(true);
+
+    expect(shallowEqual({a: 1}, {a: 2})).toBe(false);
+    expect(shallowEqual({a: {}}, {a: {}})).toBe(false);
+    expect(shallowEqual({a: b}, {a: {}})).toBe(false);
+  });
+
   it('should flat array properly', () => {
     expect(flat([1], Infinity)).toEqual([1]);
     expect(flat([[1]], Infinity)).toEqual([1]);
--- src/lab/utils.js
+++ src/lab/utils.js
@@ -2,7 +2,7 @@
 
 const shallowEqual = (a, b) => {
   if (!a || !b || typeof a !== 'object' || typeof b !== 'object') {
-    return a === b;
+    return a === b || (a === undefined && b === null) || (a === null && b === undefined);
   }
   return a === b || !Object.keys(Object.assign({}, a, b)).find(key => a[key] !== b[key]);
 };

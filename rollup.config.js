38f44862ea12e649e84fc3755e73beae9bd21cc7
Rango Yuan <yuanoook@foxmail.com>
2019-11-16 04:18:18 +0800

    resolve rollup plugins order, add umd/index.html

--- rollup.config.js
+++ rollup.config.js
@@ -12,6 +12,18 @@ export default {
     name: 'Ganic'
   },
   plugins: [
+    nodeResolve({
+      mainFields: ['module', 'main', 'browser']
+    }),
+
+    commonjs({
+      exclude: [ 'node_modules/foo/**', 'node_modules/bar/**' ],
+      extensions: [ '.js' ],
+      ignoreGlobal: false,
+      sourceMap: false,
+      ignore: [ 'conditional-runtime-dependency' ]
+    }),
+
     babel({
       exclude: 'node_modules/**'
     }),
@@ -21,38 +33,5 @@ export default {
       removeDebugger: true,
       comments: false
     }),
-
-    nodeResolve({
-      mainFields: ['module', 'main', 'browser']
-    }),
-
-    commonjs({
-      // non-CommonJS modules will be ignored, but you can also
-      // specifically include/exclude files
-      // include: 'node_modules/**',  // Default: undefined
-      exclude: [ 'node_modules/foo/**', 'node_modules/bar/**' ],  // Default: undefined
-      // these values can also be regular expressions
-      // include: /node_modules/
-
-      // search for files other than .js files (must already
-      // be transpiled by a previous plugin!)
-      extensions: [ '.js' ],  // Default: [ '.js' ]
-
-      // if true then uses of `global` won't be dealt with by this plugin
-      ignoreGlobal: false,  // Default: false
-
-      // if false then skip sourceMap generation for CommonJS modules
-      sourceMap: false,  // Default: true
-
-      // explicitly specify unresolvable named exports
-      // (see below for more details)
-      // namedExports: { 'index.js': ['create', 'attach'] },  // Default: undefined
-
-      // sometimes you have to leave require statements
-      // unconverted. Pass an array containing the IDs
-      // or a `id => boolean` function. Only use this
-      // option if you know what you're doing!
-      ignore: [ 'conditional-runtime-dependency' ]
-    })
   ]
 };

6af756b7a2c1cf85286cc9ac11dc8120c9cc2714
Rango Yuan <yuanoook@gmail.com>
2019-11-19 19:55:03 +0800

    add shared in packages dir

--- scripts/rollup/index.js
+++ scripts/rollup/index.js
@@ -7,18 +7,22 @@ const sourceDir = name => `${rootPath}/packages/${name}/`;
 const targetDir = name => `${rootPath}/build/node_modules/${name}/`;
 const buildOne = async c => await (await require('rollup').rollup(c)).write(c.output);
 const buildAll = async () => Promise.all(require('./base.config').map(buildOne));
-const syncFiles = name => bash(`cp -r ${sourceDir(name)}. ${targetDir(name)}`);
+const syncFiles = async name => {
+  await bash(`cp -r ${sourceDir(name)}. ${targetDir(name)}`);
+  await bash(`cp -r ${sourceDir(name)}../shared/. ${targetDir(name)}`);
+};
 
 const mvVersionFile = async name => {
   const dir = targetDir(name);
   const dist = `${dir}package.json`;
-  await bash(`mv ${dir}package-.json ${dist}`);
+  await bash(`mv ${dir}package.json ${dist}`);
 };
 
 const updateVersion = async name => {
   const dist = `../../build/node_modules/${name}/package.json`;
   const obj = require(dist);
   obj.version = require('../../package.json').version;
+  obj.repository.directory = obj.repository.directory.replace(/[^\/]+$/, name);
   require('fs').writeFileSync(
     require('path').resolve(__dirname, dist),
     JSON.stringify(obj, null, 2)

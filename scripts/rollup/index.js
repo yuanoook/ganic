d04c6877cc39b2b27996f837e4d9e9e1eab96295
Rango Yuan <yuanoook@foxmail.com>
2019-11-20 23:50:17 +0800

    optimize rollup

--- scripts/rollup/index.js
+++ scripts/rollup/index.js
@@ -1,5 +1,5 @@
 const {bash, getPackages} = require('../shared/utils');
-const {getConfig, externalMap} = require('./config');
+const {getConfig} = require('./config');
 const rootPath = process.cwd().replace('\\','/').replace(/^([A-Z]):/, (m,A_Z) => '/' + A_Z.toLowerCase());
 
 const sourceDir = name => `${rootPath}/packages/${name}/`;
@@ -14,27 +14,11 @@ const syncFiles = async name => {
   await bash(`cp -r ${sourceDir(name)}package.json ${targetDir(name)}package.json`);
   await Promise.all([
     'LICENSE',
-    // 'package.json',
     'readme.md',
     'index.js',
   ].map(file =>
     bash(`cp -r ${sourceDir(name)}../shared/${file} ${targetDir(name)}`)
   ));
-};
-
-const updatePackageJson = async name => {
-  const src = `../../packages/${name}/package.json`;
-  const dist = `../../build/node_modules/${name}/package.json`;
-  const obj = require(src);
-  obj.name = name;
-  obj.version = require('../../package.json').version;
-  console.log(obj);
-  // obj.repository.directory = obj.repository.directory.replace(/[^\/]+$/, name);
-  // obj.dependencies = externalMap[name].reduce((deps, depName) => ({...deps, [depName]: 'latest'}), {});
-  require('fs').writeFileSync(
-    require('path').resolve(__dirname, dist),
-    JSON.stringify(obj, null, 2)
-  );
 };
 
 const updateIndex = async name => {
@@ -48,7 +32,6 @@ const updateIndex = async name => {
 
 const afterBuild = async (name) => {
   await syncFiles(name);
-  // await updatePackageJson(name);
   await updateIndex(name);
 }
 

56df0fa541a16d656fbc8e5f69f50c9716b3d302
Rango Yuan <yuanoook@gmail.com>
2019-11-20 18:15:44 +0800

    fix postintall, unlink first :D

--- scripts/tasks/postinstall.js
+++ scripts/tasks/postinstall.js
@@ -1,13 +1,16 @@
-const {getPackages} = require('../shared/utils');
+const fs = require('fs');
 const path = require('path');
+const { getPackages } = require('../shared/utils');
 
 const source = name => path.resolve(`packages/${name}`);
 const link = name => path.resolve(`node_modules/${name}`);
-const symlink = name => require('fs').symlinkSync(source(name), link(name), 'junction');
+const unlink = name => fs.unlinkSync(link(name));
+const symlink = name => fs.symlinkSync(source(name), link(name), 'junction');
 
 const start = async () => {
   const names = await getPackages();
-  Promise.all(names.map(symlink));
+  await Promise.all(names.map(unlink));
+  await Promise.all(names.map(symlink));
 }
 
 start();

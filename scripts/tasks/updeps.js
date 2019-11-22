de97a501b61e73b0066f60925c34d6e6b99b60ac
Rango Yuan <yuanoook@foxmail.com>
2019-11-23 02:39:10 +0800

    update updeps.js, update codesandbox deps

--- scripts/tasks/updeps.js
+++ scripts/tasks/updeps.js
@@ -2,9 +2,9 @@ const fs = require('fs');
 const {getPackages} = require('../shared/utils');
 
 const getGanicVersion = () => readPackage('ganic').version;
-const readPackage = name => require(`../../packages/${name}/package.json`);
-const writePackage = (target, package) => fs.writeFileSync(
-  require('path').resolve(__dirname, `../../packages/${target}/package.json`),
+const readPackage = (name, dir = '../../packages/') => require(`${dir}${name}/package.json`);
+const writePackage = (target, package, dir = '../../packages/') => fs.writeFileSync(
+  require('path').resolve(__dirname, `${dir}${target}/package.json`),
   JSON.stringify(package, null, 2)
 );
 
@@ -13,8 +13,8 @@ const plusVersion = version => {
   return [major, minor, (+patch) + 1].join('.');
 }
 
-const updatePackageDeps = (target, deps) => {
-  const package = readPackage(target);
+const updatePackageDeps = (target, deps, dir = '../../packages/') => {
+  const package = readPackage(target, dir);
   const toUpdateKeys = Object.keys(deps).filter(key => (
     /^\d+\.\d+\.\d+$/.test(package.dependencies[key])
     && deps[key] !== package.dependencies[key])
@@ -27,7 +27,7 @@ const updatePackageDeps = (target, deps) => {
   }), {});
   package.dependencies = Object.assign({}, package.dependencies, toUpdateDeps);
   package.version = plusVersion(package.version);
-  writePackage(target, package);
+  writePackage(target, package, dir);
 }
 
 const updateDependencies = async () => {
@@ -36,4 +36,17 @@ const updateDependencies = async () => {
   names.map(name => updatePackageDeps(name, {ganic: ganicVersion}));
 }
 
-updateDependencies();
+const updateCodesandboxDeps = async () => {
+  const names = await getPackages();
+  const deps = names.reduce((prev, name) => ({
+    ...prev, [name]: readPackage(name).version
+  }),{});
+  updatePackageDeps('example', deps, `../../.codesandbox/`);
+}
+
+const start = async () => {
+  await updateDependencies();
+  await updateCodesandboxDeps();
+}
+
+start();

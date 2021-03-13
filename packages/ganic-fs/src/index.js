fc7b335c33ac107f88be304066f06b996fd3fa5a
Rango Yuan <yuanoook@foxmail.com>
2021-03-14 02:31:46 +0800

    Add ganic-fs for backend & devops :D

---
+++ packages/ganic-fs/src/index.js
@@ -0,0 +1,34 @@
+const { updateLeaf, vanishLeaf } = require('./connect/leaf');
+const { insertFile } = require('./connect/utils');
+const { organMap } = require('./connect/shared');
+const { taskify, clearTasks } = require('./taskQueue');
+const getOrganism = require('./connect/getOrganism');
+
+const setUpNode = node => {
+  const dom = organMap.get(node.organ);
+  insertFile(dom, node);
+};
+
+const onReady = taskify(setUpNode);
+const getTagkitFactory = configs => tagName => ({
+  organism: getOrganism(tagName, configs),
+  onReady,
+});
+
+const base = {
+  updateLeaf: taskify(updateLeaf),
+  vanishLeaf: taskify(vanishLeaf),
+  relocate: () => null,
+  onSettled: clearTasks,
+  onBuried: clearTasks,
+};
+
+const envFS = config => {
+  const configs = !Array.isArray(config) ? [config] : config;
+  return {
+    getTagkit: getTagkitFactory(configs),
+    ...base,
+  };
+};
+
+module.exports = envFS;
